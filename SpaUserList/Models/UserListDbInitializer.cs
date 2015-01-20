f190a0fc543f2083e59fe4969be89bd77283f977
jvujcic <jvujcic@gmail.com>
2015-01-20 15:16:17 +0100

    Seeding DB

--- SpaUserList/Models/UserListDbInitializer.cs
+++ SpaUserList/Models/UserListDbInitializer.cs
@@ -18,10 +18,28 @@ namespace SpaUserList.Models
                     Emails = new HashSet<Email>() {
                         new Email() { EmailAddress = "jvujcic@gmail.com" },
                         new Email() { EmailAddress = "jvujcic@math.hr" }
-                    } 
+                    },
+                },
+                new User() { 
+                    Name = "Mate", 
+                    Surname = "Matic", 
+                    Address = "Imotski", 
+                    Emails = new HashSet<Email>() {
+                        new Email() { EmailAddress = "mate@gmail.com" },
+                        new Email() { EmailAddress = "mate@math.hr" }
+                    },
                 }
             };
 
+            var tagBacvice = new Tag() { Name = "bacvice" };
+            var tagMath = new Tag() { Name = "math" };
+            var tagHajduk = new Tag() { Name = "hajduk" };
+
+            listOfUsers[0].Tags.Add(tagBacvice);
+            listOfUsers[0].Tags.Add(tagMath);
+            listOfUsers[1].Tags.Add(tagMath);
+            listOfUsers[1].Tags.Add(tagHajduk);
+            
             listOfUsers.ForEach(user => context.Users.Add(user));
             base.Seed(context);
         }
